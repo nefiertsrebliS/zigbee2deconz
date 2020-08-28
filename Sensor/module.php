b399076fe5dac302282ac518cc6efed0dbf3d282
Silberstreifen <silberstreifen@freenet.de>
2020-08-28 21:25:04 +0200

    V1.22 - Fehlerbehebung Cube und config.delay

--- Sensor/module.php
+++ Sensor/module.php
@@ -70,7 +70,7 @@ class Z2DSensor extends IPSModule
 
 			if($update){
 				if (property_exists($Payload, 'buttonevent')) {
-					if (property_exists($Payload, 'gesture')) {
+					if (@$Payload->gesture == 7 || @$Payload->gesture == 8) {
 						if (!IPS_VariableProfileExists('Angle.Z2D')) {
 							IPS_CreateVariableProfile('Angle.Z2D', 2);
 							IPS_SetVariableProfileIcon('Angle.Z2D', 'Repeat');
@@ -78,7 +78,7 @@ class Z2DSensor extends IPSModule
 							IPS_SetVariableProfileDigits('Angle.Z2D', 2);
 						}
 						$this->RegisterVariableFloat('Z2D_angle', $this->Translate('Angle'), 'Angle.Z2D');
-						SetValue($this->GetIDForIdent('Z2D_angle'), round($Payload->buttonevent / 10 ,2));
+						$this->SetValue('Z2D_angle', round($Payload->buttonevent / 100 ,2));
 					}else{
 						$this->RegisterVariableInteger('Z2D_Event', $this->Translate('Event'), '');
 						$this->SetValue('Z2D_Event', $Payload->buttonevent);
@@ -103,64 +103,77 @@ class Z2DSensor extends IPSModule
 						}
 
 						$this->RegisterVariableInteger('Z2D_Button_'.$button, $this->Translate('Button')." ".$button, 'ButtonEvent.Z2D');
-						SetValue($this->GetIDForIdent('Z2D_Button_'.$button), $state);
+						$this->SetValue('Z2D_Button_'.$button, $state);
 					}
 				}
 				if (property_exists($Payload, 'gesture')) {
-					$this->RegisterVariableInteger('Z2D_Gesture', $this->Translate('Gesture'), '');
-					SetValue($this->GetIDForIdent('Z2D_Gesture'), $Payload->gesture);
+                    if (!IPS_VariableProfileExists('Gesture.Z2D')) {
+                        IPS_CreateVariableProfile('Gesture.Z2D', 1);
+                        IPS_SetVariableProfileIcon('Gesture.Z2D', 'Repeat');
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 0, $this->Translate('Move'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 1, $this->Translate('Shake'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 2, $this->Translate('Drop'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 3, $this->Translate('Tilt').' 90°', '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 4, $this->Translate('Tilt').' 180°', '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 5, $this->Translate('Move'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 6, '2x '.$this->Translate('Knock'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 7, $this->Translate('Rotate Clockwise'), '',-1);
+                        IPS_SetVariableProfileAssociation('Gesture.Z2D', 8, $this->Translate('Rotate Counter Clockwise'), '',-1);
+                    }
+                    $this->RegisterVariableInteger('Z2D_Gesture', $this->Translate('Gesture'), 'Gesture.Z2D');
+					$this->SetValue('Z2D_Gesture', $Payload->gesture);
 				}
 				if (property_exists($Payload, 'carbonmonoxide')) {
 					$this->RegisterVariableBoolean('Z2D_Carbonmonoxide', $this->Translate('Carbonmonoxide'), '~Alert');
-					SetValue($this->GetIDForIdent('Z2D_Carbonmonoxide'), $Payload->carbonmonoxide);
+					$this->SetValue('Z2D_Carbonmonoxide', $Payload->carbonmonoxide);
 				}
 				if (property_exists($Payload, 'dark')) {
 					$this->RegisterVariableBoolean('Z2D_dark', $this->Translate('dark'), '~Switch');
-					SetValue($this->GetIDForIdent('Z2D_dark'), $Payload->dark);
+					$this->SetValue('Z2D_dark', $Payload->dark);
 				}
 				if (property_exists($Payload, 'fire')) {
 					$this->RegisterVariableBoolean('Z2D_fire', $this->Translate('Fire'), '~Alert');
-					SetValue($this->GetIDForIdent('Z2D_fire'), $Payload->fire);
+					$this->SetValue('Z2D_fire', $Payload->fire);
 				}
 				if (property_exists($Payload, 'daylight')) {
 					$this->RegisterVariableBoolean('Z2D_daylight', $this->Translate('Daylight'), '~Switch');
-					SetValue($this->GetIDForIdent('Z2D_daylight'), $Payload->daylight);
+					$this->SetValue('Z2D_daylight', $Payload->daylight);
 				}
 				if (property_exists($Payload, 'lowbattery')) {
 					$this->RegisterVariableBoolean('Z2D_lowbattery', $this->Translate('Battery'), '~Battery');
-					SetValue($this->GetIDForIdent('Z2D_lowbattery'), $Payload->lowbattery);
+					$this->SetValue('Z2D_lowbattery', $Payload->lowbattery);
 				}
 				if (property_exists($Payload, 'presence')) {
 					$this->RegisterVariableBoolean('Z2D_presence', $this->Translate('Presence'), '~Presence');
-					SetValue($this->GetIDForIdent('Z2D_presence'), $Payload->presence);
+					$this->SetValue('Z2D_presence', $Payload->presence);
 				}
 				if (property_exists($Payload, 'open')) {
 					$this->RegisterVariableBoolean('Z2D_open', $this->Translate('open'), '~Window');
-					SetValue($this->GetIDForIdent('Z2D_open'), $Payload->open);
+					$this->SetValue('Z2D_open', $Payload->open);
 				}
 				if (property_exists($Payload, 'on')) {
 					$this->RegisterVariableBoolean('Z2D_on', $this->Translate('on'), '~Switch');
-					SetValue($this->GetIDForIdent('Z2D_on'), $Payload->on);
+					$this->SetValue('Z2D_on', $Payload->on);
 				}
 				if (property_exists($Payload, 'tampered')) {
 					$this->RegisterVariableBoolean('Z2D_tampered', $this->Translate('tampered'), '~Alert');
-					SetValue($this->GetIDForIdent('Z2D_tampered'), $Payload->tampered);
+					$this->SetValue('Z2D_tampered', $Payload->tampered);
 				}
 				if (property_exists($Payload, 'water')) {
 					$this->RegisterVariableBoolean('Z2D_water', $this->Translate('Water'), '~Alert');
-					SetValue($this->GetIDForIdent('Z2D_water'), $Payload->water);
+					$this->SetValue('Z2D_water', $Payload->water);
 				}
 				if (property_exists($Payload, 'vibration')) {
 					$this->RegisterVariableBoolean('Z2D_vibration', $this->Translate('Vibration'), '~Alert');
-					SetValue($this->GetIDForIdent('Z2D_vibration'), $Payload->vibration);
+					$this->SetValue('Z2D_vibration', $Payload->vibration);
 				}
 				if (property_exists($Payload, 'orientation')) {
 					$this->RegisterVariableString('Z2D_orientation', $this->Translate('Orientation'), '');
-					SetValue($this->GetIDForIdent('Z2D_orientation'), json_encode($Payload->orientation));
+					$this->SetValue('Z2D_orientation', json_encode($Payload->orientation));
 				}
 				if (property_exists($Payload, 'vibrationstrength')) {
 					$this->RegisterVariableInteger('Z2D_vibrationstrength', $this->Translate('Vibrationstrength'), '');
-					SetValue($this->GetIDForIdent('Z2D_vibrationstrength'), $Payload->vibrationstrength);
+					$this->SetValue('Z2D_vibrationstrength', $Payload->vibrationstrength);
 				}
 				if (property_exists($Payload, 'tiltangle')) {
 					if (!IPS_VariableProfileExists('Angle.Z2D')) {
@@ -171,43 +184,43 @@ class Z2DSensor extends IPSModule
 					}
 
 					$this->RegisterVariableInteger('Z2D_tiltangle', $this->Translate('Tiltangle'), 'Angle.Z2D');
-					SetValue($this->GetIDForIdent('Z2D_tiltangle'), $Payload->tiltangle);
+					$this->SetValue('Z2D_tiltangle', $Payload->tiltangle);
 				}
 				if (property_exists($Payload, 'humidity')) {
 					$this->RegisterVariableFloat('Z2D_humidity', $this->Translate('Humidity'), '~Humidity.F');
-					SetValue($this->GetIDForIdent('Z2D_humidity'), $Payload->humidity / 100.0);
+					$this->SetValue('Z2D_humidity', $Payload->humidity / 100.0);
 				}
 				if (property_exists($Payload, 'lux')) {
 					$this->RegisterVariableFloat('Z2D_lux', $this->Translate('Illumination'), '~Illumination.F');
-					SetValue($this->GetIDForIdent('Z2D_lux'), $Payload->lux);
+					$this->SetValue('Z2D_lux', $Payload->lux);
 				}
 				if (property_exists($Payload, 'lightlevel')) {
 					$this->RegisterVariableFloat('Z2D_lightlevel', $this->Translate('Illumination'), '~Illumination.F');
-					SetValue($this->GetIDForIdent('Z2D_lightlevel'), $Payload->lightlevel);
+					$this->SetValue('Z2D_lightlevel', $Payload->lightlevel);
 				}
 				if (property_exists($Payload, 'pressure')) {
 					$this->RegisterVariableFloat('Z2D_pressure', $this->Translate('Airpressure'), '~AirPressure.F');
-					SetValue($this->GetIDForIdent('Z2D_pressure'), $Payload->pressure);
+					$this->SetValue('Z2D_pressure', $Payload->pressure);
 				}
 				if (property_exists($Payload, 'temperature')) {
 					$this->RegisterVariableFloat('Z2D_temperature', $this->Translate('Temperature'), '~Temperature');
-					SetValue($this->GetIDForIdent('Z2D_temperature'), $Payload->temperature / 100.0);
+					$this->SetValue('Z2D_temperature', $Payload->temperature / 100.0);
 				}
 				if (property_exists($Payload, 'consumption')) {
 					$this->RegisterVariableFloat('Z2D_consumption', $this->Translate('Consumption'), '~Electricity');
-					SetValue($this->GetIDForIdent('Z2D_consumption'), round($Payload->consumption / 1000 ,3));
+					$this->SetValue('Z2D_consumption', round($Payload->consumption / 1000 ,3));
 				}
 				if (property_exists($Payload, 'power')) {
 					$this->RegisterVariableFloat('Z2D_power', $this->Translate('Power'), '~Watt.14490');
-					SetValue($this->GetIDForIdent('Z2D_power'), $Payload->power);
+					$this->SetValue('Z2D_power', $Payload->power);
 				}
 				if (property_exists($Payload, 'voltage')) {
 					$this->RegisterVariableFloat('Z2D_voltage', $this->Translate('Voltage'), '~Volt');
-					SetValue($this->GetIDForIdent('Z2D_voltage'), $Payload->voltage);
+					$this->SetValue('Z2D_voltage', $Payload->voltage);
 				}
 				if (property_exists($Payload, 'valve')) { 
 					$this->RegisterVariableInteger('Z2D_valve', $this->Translate('Valve'), '~Intensity.255'); 
-					SetValue($this->GetIDForIdent('Z2D_valve'), $Payload->valve);
+					$this->SetValue('Z2D_valve', $Payload->valve);
 				}
 				if (property_exists($Payload, 'current')) {
 					if (!IPS_VariableProfileExists('Ampere.Z2D')) {
@@ -219,7 +232,7 @@ class Z2DSensor extends IPSModule
 
 					$this->RegisterVariableFloat('Z2D_current', $this->Translate('Current'), 'Ampere.Z2D');
 
-					SetValue($this->GetIDForIdent('Z2D_current'), round($Payload->current / 1000 ,3));
+					$this->SetValue('Z2D_current', round($Payload->current / 1000 ,3));
 				}
 				if (property_exists($Payload, 'reachable')) {
 					if($Payload->reachable){
@@ -230,7 +243,7 @@ class Z2DSensor extends IPSModule
 				}
 				if (property_exists($Payload, 'battery')) {
 					$this->RegisterVariableInteger('Z2D_Battery', $this->Translate('Battery'), '~Battery.100');
-					SetValue($this->GetIDForIdent('Z2D_Battery'), $Payload->battery);
+					$this->SetValue('Z2D_Battery', $Payload->battery);
 				}
 			}
 		}
@@ -239,7 +252,7 @@ class Z2DSensor extends IPSModule
 			$Payload = $data->config;
 			if (property_exists($Payload, 'battery')) {
 			    $this->RegisterVariableInteger('Z2D_Battery', $this->Translate('Battery'), '~Battery.100');
-			    SetValue($this->GetIDForIdent('Z2D_Battery'), $Payload->battery);
+			    $this->SetValue('Z2D_Battery', $Payload->battery);
 			}
 			if (property_exists($Payload, 'reachable')) {
 				if($Payload->reachable){
@@ -250,17 +263,17 @@ class Z2DSensor extends IPSModule
 			}
 			if (property_exists($Payload, 'temperature')) {
 			    $this->RegisterVariableFloat('Z2D_temperature', $this->Translate('Temperature'), '~Temperature');
-			    SetValue($this->GetIDForIdent('Z2D_temperature'), $Payload->temperature / 100.0);
+			    $this->SetValue('Z2D_temperature', $Payload->temperature / 100.0);
 			}
 			if (property_exists($Payload, 'heatsetpoint')) {
 			    $this->RegisterVariableFloat('Z2D_heatsetpoint', $this->Translate('Heat Setpoint'), '~Temperature');
 	            $this->EnableAction('Z2D_heatsetpoint');
-			    SetValue($this->GetIDForIdent('Z2D_heatsetpoint'), $Payload->heatsetpoint / 100.0);
+			    $this->SetValue('Z2D_heatsetpoint', $Payload->heatsetpoint / 100.0);
 			}
 			if (property_exists($Payload, 'offset')) {
 			    $this->RegisterVariableFloat('Z2D_offset', $this->Translate('Offset'), '~Temperature');
 	            $this->EnableAction('Z2D_offset');
-			    SetValue($this->GetIDForIdent('Z2D_offset'), $Payload->offset / 100.0);
+			    $this->SetValue('Z2D_offset', $Payload->offset / 100.0);
 			}
 			if (property_exists($Payload, 'delay')) {
 				if (!IPS_VariableProfileExists('Delay.Z2D')) {
@@ -271,7 +284,7 @@ class Z2DSensor extends IPSModule
 				}
 			    $this->RegisterVariableInteger('Z2D_delay', $this->Translate('Switch off Hesitation'), 'Delay.Z2D');
 	            $this->EnableAction('Z2D_delay');
-			    SetValue($this->GetIDForIdent('Z2D_delay'), $Payload->delay);
+			    $this->SetValue('Z2D_delay', $Payload->delay);
 			}
 			if (property_exists($Payload, 'sensitivitymax')) {
 			    $this->RegisterVariableInteger('Z2D_sensitivitymax', 'max. '.$this->Translate('Sensitivity'), '');
